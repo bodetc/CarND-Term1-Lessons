f26ef08cf8f96486e9a2425f5731c59f7d06aa3a
Cédric Bodet <cedric.bodet@tngtech.com>
2017-02-02 20:24:50 +0100

    Stochastic Gradient Descent

--- miniflow.py
+++ miniflow.py
@@ -290,3 +290,16 @@ def forward_and_backward(graph):
     # see: https://docs.python.org/2.3/whatsnew/section-slices.html
     for n in graph[::-1]:
         n.backward()
+
+def sgd_update(trainables, learning_rate=1e-2):
+    """
+    Updates the value of each trainable with SGD.
+
+    Arguments:
+
+        `trainables`: A list of `Input` Nodes representing weights/biases.
+        `learning_rate`: The learning rate.
+    """
+    # TODO: update all the `trainables` with SGD
+    for trainable in trainables:
+        trainable.value -= learning_rate * trainable.gradients[trainable]
